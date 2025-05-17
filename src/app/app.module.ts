febb19b661f56d7a1163b6bc813206ddba5985b8
RicDev1167 <ricardo_29leyva@outlook.com>
2025-05-17 15:09:32 -0600

    Se agrega sidebar y diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
+
+//Primero importar modules y luego componentes
 
 @NgModule({
   declarations: [
@@ -10,7 +16,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GifsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
